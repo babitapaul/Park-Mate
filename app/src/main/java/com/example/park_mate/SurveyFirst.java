1814e6889f7ef3e408dce00578b7d016873998c5
Amninderkaursidhu <Amaninderkaursidhu@gmail.com>
2020-05-28 21:32:05 -0400

    survey

--- app/src/main/java/com/example/park_mate/SurveyFirst.java
+++ app/src/main/java/com/example/park_mate/SurveyFirst.java
@@ -2,8 +2,32 @@ package com.example.park_mate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 public class SurveyFirst extends AppCompatActivity {
 
+    Button b1;
+    TextView back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey_first);
+        b1=(Button)findViewById(R.id.b1);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i= new Intent(SurveyFirst.this,SurveyTwo.class);
+                startActivity(i);
+            }
+        });
+
+        
+    }
 
     }
 
---
+++ app/src/main/java/com/example/park_mate/SurveyTwo.java
@@ -0,0 +1,14 @@
+package com.example.park_mate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SurveyTwo extends AppCompatActivity {
+
+
+}
