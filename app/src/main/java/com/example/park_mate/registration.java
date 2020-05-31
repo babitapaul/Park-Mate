3655a5c7f7149268999cd01e232a0bd9ca38153a
amninderkaursidhu <amaniderkaursidhu@gmail.com>
2020-05-31 11:40:44 -0400

    regupdate

--- app/src/main/java/com/example/park_mate/registration.java
+++ app/src/main/java/com/example/park_mate/registration.java
@@ -11,6 +11,53 @@ public class registration {
         this.mobileno=mobileno;
         this.address=address;
     }
+    public String getEmailid() {
+        return emailid;
+    }
+
+    public void setEmailid(String emailid) {
+        this.emailid = emailid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getMobileno() {
+        return mobileno;
+    }
+
+    public void setMobileno(String mobileno) {
+        this.mobileno = mobileno;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        address = address;
+    }
 
 
 }
