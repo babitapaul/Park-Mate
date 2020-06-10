a79895426bdf4c68fb04b88040737227a099fcc8
amninderkaursidhu <amaniderkaursidhu@gmail.com>
2020-06-10 00:58:39 -0400

    Manage Survey(Admin) Add, Delete, Update Design done

---
+++ app/src/main/java/com/example/park_mate/DeleteSurvey.java
@@ -0,0 +1,9 @@
+package com.example.park_mate;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DeleteSurvey extends AppCompatActivity {
+
+
+}
--- app/src/main/java/com/example/park_mate/fragmentManagesurvey.java
+++ app/src/main/java/com/example/park_mate/fragmentManagesurvey.java
@@ -1,8 +1,65 @@
 package com.example.park_mate;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class fragmentManagesurvey extends Fragment {
 
+    Button add,dt,up;
+    EditText question;
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_managesurveys,container,false);
+        add=(Button)view.findViewById(R.id.add);
+        dt=(Button)view.findViewById(R.id.delete);
+        question=(EditText)view.findViewById(R.id.surveryquestion);
+        up=(Button)view.findViewById(R.id.modify);
+        dt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(),DeleteSurvey.class);
+
+                startActivity(i);
+            }
+        });
+        up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getContext(),UpdateSurvey.class);
+
+                startActivity(i);
+            }
+        });
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (TextUtils.isEmpty(question.getText().toString().trim()))
+                {
+                    Toast toast = Toast.makeText(getContext(), "Please Enter Survey Question!", Toast.LENGTH_SHORT);
+                    toast.setMargin(50,50);
+                    toast.show();
+
+                    return;
+                }
+                Intent i = new Intent(getContext(),Add_Survery.class);
+                i.putExtra("Question",question.getText().toString());
+                startActivity(i);
+            }
+        });
+        return view;
+    }
 }
