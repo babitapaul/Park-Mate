197c05232ccf9e683af5854cabf8de69ca636ebf
Jaskaran <jaskarans260@gmail.com>
2020-06-14 00:13:00 -0400

    Welcome page xml and java complete

--- app/src/main/java/com/example/park_mate/welcomepage.java
+++ app/src/main/java/com/example/park_mate/welcomepage.java
@@ -1,14 +1,120 @@
 package com.example.park_mate;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
 
-public class welcomepage extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class welcomepage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener,manageuserfragment.onFragmentBtnSelected{
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcomepage);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+
+        //load default fragment
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.container_fragment,new dashboardfragment());
+        fragmentTransaction.commit();
+
+
+
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        if(menuItem.getItemId()== R.id.Dashboard){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment, new dashboardfragment());
+            fragmentTransaction.commit();
+
+        }
+
+        if (menuItem.getItemId() == R.id.users) {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment, new manageuserfragment());
+            fragmentTransaction.commit();
+        }
+        if (menuItem.getItemId() == R.id.parks) {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment, new fragmentmanageparks());
+            fragmentTransaction.commit();
+        }
+        if(menuItem.getItemId()== R.id.survey){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment,new fragmentManagesurvey());
+            fragmentTransaction.commit();
+
+        }
+        if(menuItem.getItemId()== R.id.post){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_fragment,new managepostfragment());
+            fragmentTransaction.commit();
+
+        }
+        if(menuItem.getItemId()==R.id.adminlogout)
+        {
+            startActivity(new Intent(getApplicationContext(),Login.class));
+        }
+
+        return true;
+
+    }
+
+
+    @Override
+    public void onButtonSelected() {
+        fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container_fragment, new dashboardfragment());
+        fragmentTransaction.commit();
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(keyCode==KeyEvent.KEYCODE_BACK) {
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
